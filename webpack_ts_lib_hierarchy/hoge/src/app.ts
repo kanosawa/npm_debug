9952c4ae160b57795c25d569dc045f5f98909fde
kanosawa <kanokanosawasawa@gmail.com>
2024-01-25 23:09:31 +0900

    sourceMapPathOverridesを指定

--- webpack_ts_lib_hierarchy/hoge/src/app.ts
+++ webpack_ts_lib_hierarchy/hoge/src/app.ts
@@ -1,17 +1,10 @@
 import { add, sub } from './math';
 
-// export function calc(): void {
-//   const a: number = 400;
-//   const b: number = 600;
-//   const coupon: number = 300;
-//   const total: number = add(a, b);
-//   const final: number = sub(total, coupon);
-//   document.body.textContent = `Your final total is ${final}`;
-// }
-
-const a: number = 400;
-const b: number = 600;
-const coupon: number = 300;
-const total: number = add(a, b);
-const final: number = sub(total, coupon);
-document.body.textContent = `Your final total is ${final}`;
+export function calc() : void {
+  const a: number = 400;
+  const b: number = 600;
+  const coupon: number = 300;
+  const total: number = add(a, b);
+  const final: number = sub(total, coupon);
+  document.body.textContent = `Your final total is ${final}`;
+};

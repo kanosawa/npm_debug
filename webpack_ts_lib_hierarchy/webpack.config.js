bd5c0fd3b12ffceb495c91c314c5d25eccbbbab0
kanosawa <kanokanosawasawa@gmail.com>
2024-01-25 20:19:07 +0900

    webpack_ts_lib_hierarchyを追加

---
+++ webpack_ts_lib_hierarchy/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'myLibrary',
+    libraryTarget: 'umd'
+  },
+  devtool: 'source-map',
+  mode: 'development'
+};
